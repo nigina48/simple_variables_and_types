b15061cf0d87e506f4693d5525da8ff7ce9fa62c
Nigina <niginabultakova@gmail.com>
2025-06-19 11:30:32 +0500

    khusanovna

--- task1.py
+++ task1.py
@@ -3,6 +3,10 @@
 # Create variables for name and age
 # Your code here: Create a variable called 'name' and assign your name to it
 # Your code here: Create a variable called 'age' and assign your age to it
+name = 'nigina'
+age = 8
+print(" my name is" , name, "I am " , age, "years old")
+
 
 # Print the information in the required format
 # Your code here: Print "My name is [name] I am [age] years old."
